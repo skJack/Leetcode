201fda454a93b465136c9c1be4b058b0016b748b
sk <514663354@qq.com>
2020-02-03 11:52:29 +0800

    Update 279.完全平方数.cpp

--- dp/279.完全平方数.cpp
+++ dp/279.完全平方数.cpp
@@ -34,4 +34,46 @@ public:
         }
         return dp[n];
     }
+    struct Node
+    {
+        int val;
+        int step;
+        Node (int v,int s)
+        {
+            val=v;
+            step = s;
+        }
+    };
+    //方法二：BFS遍历树，注意因为要求树高，所以要设置一个结构体来保存每个节点的高度
+    int numSquares(int n)
+    {
+        queue<Node> q;
+        set<int>visit;
+        visit.insert(n);
+        Node node(n,0);
+        q.push(node);
+        while(!q.empty())
+        {
+            Node root = q.front();
+            q.pop();
+            int current_step = root.step;
+            if(root.val==0)
+                return current_step;
+            int j;
+            for(j=1;root.val>=j*j;j++)
+            {
+                int base = root.val-j*j;
+                if(visit.find(base)==visit.end())
+                {
+                    //没找到
+                    visit.insert(base);
+                    Node tmp(base,current_step+1);
+                    q.push(tmp);
+                }
+                //cout<<root-j*j<<endl;
+            }
+        }
+        return -1;
+        
+    }
 };
