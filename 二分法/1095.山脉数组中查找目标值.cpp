a86f9bb46be1b55abc56b2f7f25c78e0d0e1ccc4
sk <514663354@qq.com>
2020-04-30 00:02:54 +0800

    Update 1095.山脉数组中查找目标值.cpp

--- 二分法/1095.山脉数组中查找目标值.cpp
+++ 二分法/1095.山脉数组中查找目标值.cpp
@@ -49,3 +49,59 @@ MountainArray.length() - 会返回该数组的长度
 0 <= target <= 10^9
 0 <= mountain_arr.get(index) <= 10^9
 */
+
+class Solution {
+public:
+    //和116题一样
+    int findPeak(MountainArray &mountainArr)
+    {
+        int len = mountainArr.length();
+        //先找山顶元素,也就是最大值
+        int left = 0,right = len-1;
+        
+        while(left<right)
+        {
+            int mid = (left+right)/2;
+            if(mountainArr.get(mid)<mountainArr.get(mid+1))
+            {
+                //此时处于上升序列，顶端元素在右边
+                left = mid+1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        return right;
+    }
+    int binary_search(int target, MountainArray &mountainArr,int l,int r)
+    {
+        while(l<=r)
+        {
+            int mid = (l+r)/2;
+            int cur = mountainArr.get(mid);
+            if(cur==target)
+                return mid;
+            else if(cur<target)
+            {
+                l = mid+1;
+            }
+            else
+            {
+                r = mid-1;
+            }
+        }
+        return -1;
+    }
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int len = mountainArr.length();
+        int top = findPeak(mountainArr);
+        int l = binary_search(target,mountainArr,0,top);
+        if(l!=-1)
+            return l;
+        else
+        {
+            return binary_search(target,mountainArr,top+1,len-1);
+        }
+    }
+};
