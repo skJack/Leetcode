f5013db9becfb1541385b0b15b89f7d4ff2d808a
sk <514663354@qq.com>
2020-05-08 23:21:08 +0800

    Update 221.最大正方形.cpp

--- dp/221.最大正方形.cpp
+++ dp/221.最大正方形.cpp
@@ -56,4 +56,36 @@ public:
         }
         return maxlen*maxlen;
     }
+    //这个题如何定义dp很重要
+    //d[i][j]表示坐标点i,j为右下角的最大正方形
+    //如果说你的 d[i,j] 是想 这个区域中 最大的正方形，那可就麻烦太多了。
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if(matrix.empty() || matrix[0].empty()) return 0;
+
+        int rows = matrix.size(), cols = matrix[0].size();
+        vector<vector<int >> dp(rows, vector<int>(cols,0));     // dp[i][j]表示以坐标i,j作为右下角的最大矩形边长
+        int maxLen = 0;
+        // 初始化第一列
+        for (int i = 0; i < rows; ++i) {
+            dp[i][0] = matrix[i][0] - '0';
+            maxLen = max(maxLen,dp[i][0]);
+        }
+        // 初始化第一行
+        for (int j = 0; j < cols; ++j) {
+            dp[0][j] = matrix[0][j] - '0';
+            maxLen = max(maxLen,dp[0][j]);
+        }//需要初始化
+        for (int i = 1; i < rows; ++i) {
+            for (int j = 1; j < cols; ++j) {
+                if (matrix[i][j]=='0') continue;  // 面积为0
+                int len1 = dp[i-1][j];
+                int len2 = dp[i][j-1];
+                int len3 = dp[i-1][j-1];
+                dp[i][j] = min(min(len1,len2),len3) + 1;    // 满足条件的最大边长
+                maxLen = max(dp[i][j], maxLen);
+            }
+        }
+        return maxLen*maxLen;
+    }
+
 };
