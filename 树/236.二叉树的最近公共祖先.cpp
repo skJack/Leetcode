aeca2f3ef215d7f2f95724adcfe5f80e0cb2dc5c
sk <514663354@qq.com>
2020-05-11 00:24:47 +0800

    Update 236.二叉树的最近公共祖先.cpp

--- 树/236.二叉树的最近公共祖先.cpp
+++ 树/236.二叉树的最近公共祖先.cpp
@@ -31,3 +31,26 @@ Companies
 所有节点的值都是唯一的。
 p、q 为不同节点且均存在于给定的二叉树中。
 */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL)
+            return NULL;
+        if(root == p || root == q) 
+            return root;
+            
+        TreeNode* left =  lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+       
+        if(left == NULL)
+            return right;
+        if(right == NULL)
+            return left;      
+        if(left && right) // p和q在两侧
+            return root;
+        
+        return NULL; // 必须有返回值
+        
+    }
+};
