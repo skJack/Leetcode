a37e68f36cb608284ae0f988250a1540d5f71b33
sk <514663354@qq.com>
2020-05-21 17:01:49 +0800

    Update and rename 5.最长回文串.cpp to 5.(dp.中心扩展)最长回文串.cpp

--- 字符串/5.(dp.中心扩展)最长回文串.cpp
+++ 字符串/5.(dp.中心扩展)最长回文串.cpp
@@ -55,4 +55,31 @@ public://从中心判断是否是回文串并返回最大长度
         //cout<<max_center<<" "<<ans;
         return s.substr(start,end-start+1);
     }
+
+    //方法二：动态规划
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        string ans;
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                //初始状态dp[i][i] = 1,dp[i][i+1] = (s[i] == s[j])
+                if (l == 0) {
+                    dp[i][j] = 1;
+                }
+                else if (l == 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                }
+                
+                else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.size()) {
+                    ans = s.substr(i, l + 1);
+                }
+            }
+        }
+        return ans;
+    }
 };
