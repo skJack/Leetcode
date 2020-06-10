0bec0ff608692422f05d3052ad55a3a0a7f21f2e
sk <514663354@qq.com>
2020-06-10 21:27:55 +0800

    Update 面试题29.顺时针打印数组.cpp

--- 序列/面试题29.顺时针打印数组.cpp
+++ 序列/面试题29.顺时针打印数组.cpp
@@ -70,4 +70,29 @@ public:
             } 
             return ans;
     }
+ 
+    //方法二：不撞南墙不回头。写了半天没写出来，边界控制好麻烦。学习一下先判断，再更改的操作
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int x_dir[4] = {1,0,-1,0};
+        int y_dir[4] = {0,1,0,-1};//从左到右
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<int>ans(col*row);
+        vector<vector<int>>visit(row,vector<int>(col,0));
+        int x=0,y=0;
+        int direction_index=0;
+        for(int i=0;i<col*row;i++)
+        {
+            ans[i] = matrix[y][x];
+            visit[y][x] = 1;
+            int xx = x+x_dir[direction_index];
+            int yy = y+y_dir[direction_index];
+            if(xx<0 || xx>=row || yy<0 || yy>=col || visit[xx][yy]==1)//当到达边界的时候或者已经遍历过的时候.注意要先判断再转移
+                direction_index = (direction_index+1)%4;   
+            x+=x_dir[direction_index];
+            y+=y_dir[direction_index];
+            cout<<ans[i]<<" "<<direction_index<<" "<<x<<" "<<y<<endl;
+        }
+        return ans;
+    }
 };
